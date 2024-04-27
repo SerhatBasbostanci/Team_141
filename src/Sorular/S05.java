87864fb1b19f82a3f794ef4997e74b66792920ac
serhat <topraksu0990@gmail.com>
2024-04-27 21:45:14 +0300

    version

---
+++ src/Sorular/S05.java
@@ -0,0 +1,11 @@
+package Sorular;
+
+import java.util.Scanner;
+
+public class S05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner ( System.in );
+        System.out.println ("Adinizi giriniz");
+        String isim = scan.nextLine ();
+    }
+}
